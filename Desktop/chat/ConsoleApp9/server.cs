3b135cc47c30f05e46cd8e76e06352aee3350027
youssefgamal111 <youssefuture@outlook.com>
2022-05-15 20:53:13 +0200

    upload

---
+++ Desktop/chat/ConsoleApp9/server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace ConsoleApp9
+{
+    class server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 8000);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(ipEnd);
+            sock.Listen(100);
+            Socket clientsock = sock.Accept();
+             string filename = args[0];
+            Console.WriteLine("youssef gamal abdelnasser\n CS\n 5 ");
+                Console.WriteLine("sending file: "+filename);
+                byte [] arr = new byte[1024];
+                byte[] sizearr = BitConverter.GetBytes(filename.Length);
+              
+                sizearr.CopyTo(arr, 0);
+                sizearr = Encoding.ASCII.GetBytes(filename);
+                sizearr.CopyTo(arr,4);
+            sizearr = File.ReadAllBytes(filename);
+                sizearr.CopyTo(arr, 4+filename.Length);
+                clientsock.Send(arr);
+            clientsock.Shutdown(SocketShutdown.Both);
+            clientsock.Close();
+            Console.WriteLine("File Sent");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Desktop/chat/client.cs/client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace client.cs
+{
+    class client
+    {
+        static void Main(string[] args)
+        {
+             IPAddress host = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ip = new IPEndPoint(host, 8000);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(ip);
+               
+                byte[] receivedData = new byte[1024];
+                int receivedBytesLen = sock.Receive(receivedData);
+                int filenameLength = BitConverter.ToInt32(receivedData,0);
+            string fname = Encoding.ASCII.GetString(receivedData, 4, filenameLength);
+            var fdata = Encoding.ASCII.GetString(receivedData,4+filenameLength, receivedBytesLen-4-filenameLength);
+            File.WriteAllText(fname, fdata);
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+
+        }
+    }
+}
